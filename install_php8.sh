166766f81dcd18b540d3dbbc97d46761cc5fd3ef
255cloudy <collorucha@gmail.com>
2022-02-07 20:14:57 +0300

    composer install

--- install_php8.sh
+++ install_php8.sh
@@ -5,3 +5,14 @@ sudo add-apt-repository ppa:ondrej/php
 # ensure apt is ready to installl
 sudo apt update
 sudo apt install php8.0
+
+#installing composer
+sudo apt install curl php-cli php-mbstring php-curl git unzip
+cd ~
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+
+sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+
+#in case of problems 
+sudo apt install php-xml
+composer require phpunit/phpunit
